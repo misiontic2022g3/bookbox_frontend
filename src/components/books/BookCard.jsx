91bc34eda0a3bd06b1563c9c39af086d39962956
Juan <jd.camayo@gmail.com>
2021-08-20 17:56:29 -0500

    fixing book card styles

--- src/components/books/BookCard.jsx
+++ src/components/books/BookCard.jsx
@@ -3,30 +3,35 @@ import { Link } from 'react-router-dom'
 function BookCard(props) {
     const { _id, title, cover, description } = props.book
     return (
-        <article className='col'>
-            <div className='card bg-dark text-light mb-5 card-book'>
-                <div className='row g-0'>
-                    <div className='col-md-4 card-book-img'>
-                        <img
-                            src={cover}
-                            className='img-fluid rounded-start'
-                            alt={title}
-                        />
-                    </div>
-                    <div className='col-md-8'>
-                        <div className='card-body card-body-book'>
-                            <h5 className='card-title text-warning'>{title}</h5>
-                            <p className='card-text'>{description}</p>
+        <article className='col d-flex justify-content-center'>
+            <div className='card bg-dark text-light mb-5'>
+                <div className='card-body container-fluid m-0 p-0'>
+                    <div className='row'>
+                        <div className='col-12 col-lg-5'>
+                            <img
+                                src={cover}
+                                className='card-img-top'
+                                alt={title}
+                            />
                         </div>
-                        <div className='card-footer text-center'>
-                            <Link
-                                to={'/book/' + _id}
-                                className='btn btn-warning mx-3 w-100'
-                            >
-                                Ver mas
-                            </Link>
+                        <div className='col-12 col-lg-7'>
+                            <div className=' card-body'>
+                                <h4 className='card-title text-warning'>
+                                    <strong>{title}</strong>
+                                </h4>
+                                <p className='card-text'>{description}</p>
+                            </div>
                         </div>
                     </div>
+                </div>
+                <div className='card-footer d-flex justify-content-center'>
+                    <Link
+                        to={'/book/' + _id}
+                        className='btn btn-warning mx-3 w-100'
+                        style={{ maxHeight: '40px' }}
+                    >
+                        Ver mas
+                    </Link>
                 </div>
             </div>
         </article>
